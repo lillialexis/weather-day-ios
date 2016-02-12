6b09d6f408f88971664dff659a22ae60386e0122
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-02-11 17:55:38 -0800

    Initial Commit

---
+++ WeatherDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherDay
+//
+//  Created by Lilli Szafranski on 2/11/16.
+//  Copyright © 2016 Lilli Szafranski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherDay
+//
+//  Created by Lilli Szafranski on 2/11/16.
+//  Copyright © 2016 Lilli Szafranski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
